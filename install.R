d32c0e3c29ebff4f9ee93995385a9343c2223cf7
Nitesh Turaga <nitesh.turaga@gmail.com>
2019-12-20 12:59:35 -0500

    Add changes to install.R

--- install.R
+++ install.R
@@ -1,19 +1,5 @@
-if ("BiocManager" %in% rownames(installed.packages()))
-    remove.packages("BiocManager")
-
 install.packages("BiocManager", repos="https://cran.rstudio.com")
 
-library(BiocManager)
+BiocManager::install(version="3.11", update=TRUE, ask=FALSE)
 
-if(BiocManager::version() != "3.11"){
-    BiocManager::install(version="3.11",
-                         update=TRUE, ask=FALSE)
-}
-
-builtins <- c("Matrix", "KernSmooth", "mgcv", "devtools")
-
-for (builtin in builtins)
-    if (!suppressWarnings(require(builtin, character.only=TRUE)))
-        suppressWarnings(BiocManager::install(builtin,
-                                              version="3.11",
-                                              update=TRUE, ask=FALSE))
+BiocManager::install('devtools')
