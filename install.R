3584776543f1d3bd9a6e763b882738a637c2b412
Nitesh Turaga <nitesh.turaga@gmail.com>
2021-08-03 22:18:12 -0400

    Enhancement: Add AnVIL package
    - Add package 'AnVIL' to Docker images to enable binary installation
    
    - AnVIL::install() - should locate binary repo by default
    
    - Version bump: Patch number 36

--- install.R
+++ install.R
@@ -2,4 +2,4 @@ install.packages("BiocManager", repos="https://cran.rstudio.com")
 
 BiocManager::install(version="3.13", update=TRUE, ask=FALSE)
 
-BiocManager::install('devtools')
+BiocManager::install(c('devtools', 'AnVIL'))
