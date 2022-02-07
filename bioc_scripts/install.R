63057690fc83c1d3b71e3b67ebbf6b54b0f7c5ff
Nitesh Turaga <nitesh.turaga@gmail.com>
2022-02-07 09:08:43 -1000

    Install AnVIL again

--- bioc_scripts/install.R
+++ bioc_scripts/install.R
@@ -2,4 +2,4 @@ install.packages("BiocManager", repos="https://cran.rstudio.com")
 
 BiocManager::install(version="3.15", update=TRUE, ask=FALSE)
 
-BiocManager::install('devtools')
+BiocManager::install(c('devtools', 'AnVIL'))
