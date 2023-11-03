dc3a617509d2f3e95da292efffafafb4d0ed90e4
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-11-03 14:48:01 -0400

    Update script with new R version patch

--- .github/scripts/rocker_prep.sh
+++ .github/scripts/rocker_prep.sh
@@ -11,14 +11,11 @@ sed -i "s#install_quarto.sh#install_quarto.sh || true#g" rocker-versioned2/docke
 
 BIOC_MINOR=$(echo "$BIOCVER" | awk -F'.' '{print $NF}')
 echo "Bioconductor Version: $BIOCVER"
-if [ "$RVER" = "devel" ];
-then
-  if [ $((BIOC_MINOR%2)) -eq 0 ];
-  then
-      echo "Using latest release R since Bioc devel version is even";
-      sed -i "s#R_VERSION=$RVER#R_VERSION=latest#g" rocker-versioned2/dockerfiles/r-ver_$RVER.Dockerfile
-  else
-      echo "Using latest pre-release R since Bioc devel version is even";
-      sed -i "s#R_VERSION=$RVER#R_VERSION=patched#g" rocker-versioned2/dockerfiles/r-ver_$RVER.Dockerfile
-  fi
+if [ "$BIOCVER" = "devel" ]; then
+    DEVEL_R_VER=$(curl https://bioconductor.org/config.yaml | grep '"$BIOCVER":' | awk '{print $2}' | sed 's/"//g')
+    REL_VER=$(curl https://cran.r-project.org/src/base/VERSION-INFO.dcf | grep "$DEVEL_R_VER" | awk -F':' '{print $1}')
+    # if the matching version is under release rather than devel, use patched pre-release rather than devel pre-release
+    if [ "$REL_VER" == "Release" ]; then 
+        sed -i 's#R_VERSION=$BIOCVER#R_VERSION=patched#g' rocker-versioned2/dockerfiles/r-ver_$BIOCVER.Dockerfile
+    fi
 fi
