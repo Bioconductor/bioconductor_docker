05d92a461ba11dd541c1222dbcf74adcf6d69b70
Alexandru Mahmoud <almahmoudresearch@gmail.com>
2024-04-17 14:22:00 -0400

    Fix patched vs devel detection for rocker builds

---
+++ .github/scripts/devel_or_patched_rversion.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+BIOCVER=$1
+FILETOPATCH=$2
+
+DEVEL_R_VER=$(curl https://bioconductor.org/config.yaml | grep '_devel:' | awk '{print $2}' | sed 's/"//g')
+REL_VER=$(curl https://cran.r-project.org/src/base/VERSION-INFO.dcf | grep "$DEVEL_R_VER" | awk -F':' '{print $1}')
+# if the matching version is not under devel, use patched pre-release rather than devel pre-release
+if [ "$REL_VER" != "Devel" ]; then 
+    sed -i 's#R_VERSION=devel#R_VERSION=patched#g' "$FILETOPATCH"
+fi
+
+cat "$FILETOPATCH"
